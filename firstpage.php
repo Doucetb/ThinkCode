acdc91ba4592155de1f20ebe26246476c627a0f5
Gabrielle Leung <gabrielle.leung@ynov.com>
2017-03-15 16:17:35 +0100

    1

---
+++ firstpage.php
@@ -0,0 +1,18 @@
+<?php include ("index.php");?>
+<?php include ("part/header.php");?>
+
+<div class="welcome-page">
+    <h1>Bienvenue sur le Job board handi-accessible de Think Code</h1>
+    <hr>
+    <p>
+        Le site d'emploi dédié aux personnes porteuses d'handicap. 
+    </p>
+    
+    <hr>
+    <a href="./register.php" class="btn btn-success">S'inscrire</a>
+    <a href="./login.php" class="btn btn-success">Se connecter</a>
+
+</div>
+
+<?php include ("part/footer.php");?>
+
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+	if(session_status() == PHP_SESSION_NONE){
+
+		session_start();
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>ThinkCode Jobboard</title>
+        <link rel="stylesheet" href="css/style.css"> 
+    </head>
+    
+  
